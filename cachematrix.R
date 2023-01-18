fb9dbb8ae6a71167ffd476d03d7c1257818a51e2
KatariinaIngerma <katariina.ingerma@gmail.com>
2023-01-18 16:55:17 +0200

    Solution to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# Very similar to makeVector example. It makes setters and getters for matrix and inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+# Makes inverse matrix with function above.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)){
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
